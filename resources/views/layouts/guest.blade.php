6b8968270f05aa6f284a82dcda5907f8136e9419
alan <Drake@DESKTOP-2C494C7>
2025-10-31 19:22:22 -0400

    cambios sin vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- CSS compilado -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
@@ -26,5 +26,7 @@
                 {{ $slot }}
             </div>
         </div>
+        <script  src="{{ asset('js/app.js') }}"></script>
+          <script  src="{{ asset('js/bootstrap.js') }}"></script>
     </body>
 </html>
